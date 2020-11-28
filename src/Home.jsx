73872745df48fcf130be8d8daf8a0bfde96f31fa
Paul van Mierlo <pauljozefvanmierlo@yahoo.com>
2020-11-28 19:23:03 +0100

    home page ook gevuld

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './home.css'
 
 class Home extends React.Component{
-    render(){return (<h1>Home</h1>)}
+    render(){
+        return (
+        <div>
+            <br/>
+            <div className="containers"> 
+               <h1 class="bijschrift">Welkom bij tandarts wortelkanaal</h1>
+               <img src="https://www.mupload.nl/img/fjjhhy8.jpg" width="300px"/>
+            </div>
+            
+            <div className="containers">
+                <img src="https://www.mupload.nl/img/u9zq9litz.jpg" width="300px"/>
+                <h1 class="bijschrift"> Hier werken de tandartsen met plezier </h1>
+            </div> 
+            <br/>
+
+         
+        </div>
+            
+            )}
 }
 
 export default Home
